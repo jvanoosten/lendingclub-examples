47da91d01fc24477d9e36c5e7f1f00fb12d7a89c
CatherineCao2016 <caoying0824@gmail.com>
2017-08-08 22:25:42 -0400

    Update deployfuncs.py

--- deployfuncs.py
+++ deployfuncs.py
@@ -1,8 +1,6 @@
-
 import urllib3, requests, json
 
 def get_token(creds):
-    import urllib3, requests, json
     # This block gets your authorization token
     mltoken = 0
     headers = urllib3.util.make_headers(basic_auth='{}:{}'.format(creds["username"], creds["password"]))
